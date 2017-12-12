65356496ca0bee3c4569b619a292bfa9b53b3ee0
pknowledge <walriver8@gmail.com>
2017-12-12 18:46:32 -0500

    Inicio de proyecto de pruebas

---
+++ hola.php
@@ -0,0 +1,15 @@
+<!<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Prueba PHP</title>
+ </head>
+  <body>
+
+      <?php
+
+          echo "Walter es un super Campeon !!!";
+       ?>
+
+  </body>
+</html>
